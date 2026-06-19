b2de172af60d64d3b978bcb1c4ab8770ea1a0824
farisalharbi860-ship-it <farisalharbi860@gmail.com>
2026-06-19 23:19:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - مؤسسة روح المنافسة المحلية */
-const CACHE = 'rooh-app-v202';
+const CACHE = 'rooh-app-v203';
 
 /* ملفات التطبيق الأساسية + المكتبات الخارجية لتشغيله دون إنترنت */
 const ASSETS = [
